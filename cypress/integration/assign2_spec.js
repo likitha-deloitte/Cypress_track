ab10ee65f8bc56f9da869a4d33d0dbd27367453e
mulikitha <mulikitha@deloitte.com>
2022-11-17 19:01:57 +0530

    selecting dropdown elements

---
+++ cypress/integration/assign2_spec.js
@@ -0,0 +1,30 @@
+/// <reference types ="Cypress" />
+describe('AutomationDemoSite',()=>{
+
+    it('selectingLanguages',()=> {
+        cy.visit('https://demo.automationtesting.in/Register.html')
+        cy.get("#msdd").click()
+        cy.get("//a[@class='ui-corner-all']").each(function ($ele,index,$list) 
+        {
+            if($ele.text == 'Arabic')
+            {
+                cy.log("Found Element")
+                cy.wrap($ele).click()
+            }
+            else {
+                cy.log("Current Value",$ele.text())
+            }
+        })
+    })
+
+    it('SelectingSkills', ()=> {
+       cy.get("//select[@id='Skills']").select('APIs').should('have.value','APIs')
+       //cy.get('//*[@id="Skills"]').click()
+       cy.xpath("//select[@id='Skills']").select()
+    })
+
+    it('SlectingCountry', ()=> {
+        cy.get("//span[@role='combobox']").click()
+
+    })
+})
