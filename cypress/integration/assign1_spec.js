da96123be5d36071ea656d3c1132e4b4d4ae8879
mulikitha <mulikitha@deloitte.com>
2022-11-17 08:23:29 +0530

    Using Assertions

---
+++ cypress/integration/assign1_spec.js
@@ -0,0 +1,34 @@
+describe('FlipKartSite', () =>
+{
+    it('verifyingURL', () => 
+    {
+        cy.visit("http://www.flipkart.com")
+        cy.url().should('eq','https://www.flipkart.com/')
+
+    
+    })
+    it('verifyingelements', () =>
+    {
+        cy.get('div#_331-kn_2tvxW>div:nth-of-type(1)').should("have.text","Top Offers")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(2)').should("have.text","Grocery")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(3)').should("have.text","Mobiles")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(4)').should("have.text","Fashion")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(5)').should("have.text","Electronics")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(6)').should("have.text","Home")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(7)').should("have.text","Appliances")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(8)').should("have.text","Travel")
+        cy.get('div#_331-kn _2tvxW>div:nth-of-type(9)').should("have.text","Beauty, Toys & More")
+        cy.get("div#_331-kn _2tvxW>div:nth-of-type(10)").should('have.text','2-Wheelers')
+        
+    })
+    it('login', () =>
+    {
+        cy.get('#container > div > div._1kfTjk > div._1rH5Jn > div._2Xfa2_ > div.go_DOp._2errNR > div > div > div > a').click()
+        cy.get('body > div._2Sn47c > div > div > div > div > div._36HLxm.col.col-3-5 > div > form > div._1D1L_j > button').click()
+    })
+    it('verifyingError', () =>
+    {
+        cy.get('body > div._2Sn47c > div > div > div > div > div._36HLxm.col.col-3-5 > div > form > div.IiD88i._351hSN.undefined > span._2YULOR > span').should('have.text', 'Please enter valid Email ID/Mobile number')
+    })
+})
+ 
